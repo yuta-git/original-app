1f416be0b457424e60930c0b52dd3b09e051a899
Yuta Ito <yutaito@example.com>
2021-12-08 05:59:23 +0900

    修正html

--- assets/js/drawer.js
+++ assets/js/drawer.js
@@ -5,4 +5,11 @@ $(function () {
     $('.main-nav').toggleClass('open');
     $('body').toggleClass('noscroll');
   });
+  $(".js-p-function__check").on('click', function () {
+    $(this).toggleClass('opacity-off');
+  });
+  $(".js-my-star").on('click', function () {
+    $(this).toggleClass('far');
+    $(this).toggleClass('fas');
+  });
 });
--- src/js/drawer.js
+++ src/js/drawer.js
@@ -3,4 +3,14 @@ $(function () {
     $('.main-nav').toggleClass('open');
     $('body').toggleClass('noscroll');
   });
+
+  $(".js-p-function__check").on('click', function(){
+    $(this).toggleClass('opacity-off');
+  });
+
+  $(".js-my-star").on('click', function() {
+    $(this).toggleClass('far');
+    $(this).toggleClass('fas');
+  });
+
 });
